a72027bfa52f1f1cff420b916c1a15a78881b509
edgarit <e_derksen@live.de>
2020-04-03 10:47:25 +0000

    make available custom evironments
    - use shell variable GATSBY_ACTIVE_ENV
    - print used environment to console

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,8 @@
+const activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+console.log(`Using environment config: .env.'${activeEnv}'`)
+
 require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`
+  path: `.env.${activeEnv}`
 })
 
 module.exports = {
@@ -26,7 +29,7 @@ module.exports = {
         'api_key': process.env.CONTENTSTACK_API_KEY,
         'delivery_token': process.env.CONTENTSTACK_DELIVERY_TOKEN,
         'environment': process.env.CONTENTSTACK_ENVIRONMENT,
-	'cdn': process.env.CONTENTSTACK_CDN      
+	      'cdn': process.env.CONTENTSTACK_CDN      
       },
     },
     'gatsby-plugin-offline',
