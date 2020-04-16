6cdb99d4934b69c33e7bb98674d0c1f26347b7b2
edgarit <e_derksen@live.de>
2020-04-16 12:41:35 +0000

    background immage and footer added
    - add backgroundimage to improve performace
    - add footer to honor involved services

--- src/components/layout.js
+++ src/components/layout.js
@@ -42,6 +42,24 @@ const Layout = ({ children }) => (
 </div>
   </div>
 </section>
+<footer class="footer">
+  <br/>
+  <div class="content has-text-centered">
+  <b>site built using only cloud services:</b><br/><br/>
+  <a href="https://eu-app.contentstack.com/">consuming headless cms from eu host of Contentstack</a><br/>
+  <a href="https://www.gatsbyjs.org/">
+  site generator using react and graphql with Gatsby
+</a><br/>
+  spaceimage by <a href="https://pixabay.com/de/users/Sttefan-3916977/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1982212">Stefan Popovici</a> from <a href="https://pixabay.com/de/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1982212">Pixabay</a><br/>
+    <a href="https://bulma.io">
+  styles based on sass and Bulma
+</a><br/>
+<a href="https://www.netlify.com/">tested on CDN host using netlify</a><br/>
+<a href="https://github.com/">code stored on github</a><br/>
+<a href="https://repl.it/">code edited on replit</a>
+  </div>
+</footer>
+
       </>
     )}
   />
